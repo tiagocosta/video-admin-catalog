a1aa81d4fc382ed5660fd13a1358da6888f38e9f
tiagocosta <tiago.costa.silva@gmail.com>
2025-02-11 20:59:21 -0300

    Add CategoryGateway to show other modules how to access Category domain

---
+++ domain/src/main/java/com/tcs/admin/catalog/domain/category/CategoryGateway.java
@@ -0,0 +1,18 @@
+package com.tcs.admin.catalog.domain.category;
+
+import com.tcs.admin.catalog.domain.pagination.Pagination;
+
+import java.util.Optional;
+
+public interface CategoryGateway {
+
+    Category create(Category aCategory);
+
+    void deleteById(CategoryID anId);
+
+    Optional<Category> findById(CategoryID anId);
+
+    Category update(Category aCategory);
+
+    Pagination<Category> findAll(CategorySearchQuery aQuery);
+}
---
+++ domain/src/main/java/com/tcs/admin/catalog/domain/category/CategorySearchQuery.java
@@ -0,0 +1,10 @@
+package com.tcs.admin.catalog.domain.category;
+
+public record CategorySearchQuery(
+        int page,
+        int perPage,
+        String terms,
+        String sort,
+        String direction
+) {
+}
