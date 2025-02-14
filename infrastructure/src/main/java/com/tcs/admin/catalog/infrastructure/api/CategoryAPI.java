76a00faa26a647d1ccc9fc2b3ce2f1a512970d0b
tiagocosta <tiago.costa.silva@gmail.com>
2025-02-14 20:08:18 -0300

    Add springdocs openapi

--- infrastructure/src/main/java/com/tcs/admin/catalog/infrastructure/api/CategoryAPI.java
+++ infrastructure/src/main/java/com/tcs/admin/catalog/infrastructure/api/CategoryAPI.java
@@ -1,21 +1,38 @@
 package com.tcs.admin.catalog.infrastructure.api;
 
 import com.tcs.admin.catalog.domain.pagination.Pagination;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/categories")
+@Tag(name = "Categories")
 public interface CategoryAPI {
 
     @PostMapping(
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
+    @Operation(summary = "Create a new category")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Created successfully"),
+            @ApiResponse(responseCode = "422", description = "A validation error was thrown"),
+            @ApiResponse(responseCode = "500", description = "Internal server error"),
+    })
     ResponseEntity<?> createCategory();
 
     @GetMapping
+    @Operation(summary = "List all categories - filtered and paginated")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Listed successfully"),
+            @ApiResponse(responseCode = "422", description = "Invalid parameter received"),
+            @ApiResponse(responseCode = "500", description = "Internal server error"),
+    })
     Pagination<?> listCategories(
             @RequestParam(name = "search", required = false, defaultValue = "") final String search,
             @RequestParam(name = "page", required = false, defaultValue = "0") final int page,
