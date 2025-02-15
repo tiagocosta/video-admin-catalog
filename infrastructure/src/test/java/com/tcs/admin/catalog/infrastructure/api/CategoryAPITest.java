d74c01f4fd8944cfdcfaedcc9c7f1da03ae7efa6
tiagocosta <tiago.costa.silva@gmail.com>
2025-02-14 21:41:14 -0300

    Add global exception handler

--- infrastructure/src/test/java/com/tcs/admin/catalog/infrastructure/api/CategoryAPITest.java
+++ infrastructure/src/test/java/com/tcs/admin/catalog/infrastructure/api/CategoryAPITest.java
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tcs.admin.catalog.ControllerTest;
 import com.tcs.admin.catalog.application.category.create.CreateCategoryOutput;
 import com.tcs.admin.catalog.application.category.create.CreateCategoryUseCase;
+import com.tcs.admin.catalog.domain.exceptions.DomainException;
+import com.tcs.admin.catalog.domain.validation.Error;
+import com.tcs.admin.catalog.domain.validation.handler.Notification;
 import com.tcs.admin.catalog.infrastructure.category.models.CreateCategoryApiInput;
 import io.vavr.API;
 import org.hamcrest.Matchers;
@@ -61,6 +64,75 @@ public class CategoryAPITest {
         verify(createCategoryUseCase, times(1)).execute(argThat(cmd ->
                 Objects.equals(expectedName, cmd.name())
                 && Objects.equals(expectedDescription, cmd.description())
+                        && Objects.equals(expectedIsActive, cmd.isActive())
+        ));
+    }
+
+    @Test
+    public void givenInvalidName_whenCallsCreateCategory_thenReturnNotification() throws Exception {
+        final String expectedName = null;
+        final var expectedDescription = "Most watched";
+        final var expectedIsActive = true;
+        final var expectedErrorMessage = "'name' should not be null";
+
+        final var anInput =
+                new CreateCategoryApiInput(expectedName, expectedDescription, expectedIsActive);
+
+        when(createCategoryUseCase.execute(any()))
+                .thenReturn(API.Left(Notification.create(new Error(expectedErrorMessage))));
+
+        final var request = MockMvcRequestBuilders.post("/categories")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(anInput));
+
+        this.mockMvc.perform(request)
+                .andDo(print())
+                .andExpectAll(
+                        status().isUnprocessableEntity(),
+                        header().string("Location", Matchers.nullValue()),
+                        header().string("Content-Type", MediaType.APPLICATION_JSON_VALUE),
+                        jsonPath("$.errors", Matchers.hasSize(1)),
+                        jsonPath("$.errors[0].message", Matchers.equalTo(expectedErrorMessage))
+                );
+
+        verify(createCategoryUseCase, times(1)).execute(argThat(cmd ->
+                Objects.equals(expectedName, cmd.name())
+                        && Objects.equals(expectedDescription, cmd.description())
+                        && Objects.equals(expectedIsActive, cmd.isActive())
+        ));
+    }
+
+    @Test
+    public void givenInvalidCommand_whenCallsCreateCategory_thenReturnDomainException() throws Exception {
+        final String expectedName = null;
+        final var expectedDescription = "Most watched";
+        final var expectedIsActive = true;
+        final var expectedErrorMessage = "'name' should not be null";
+
+        final var anInput =
+                new CreateCategoryApiInput(expectedName, expectedDescription, expectedIsActive);
+
+        when(createCategoryUseCase.execute(any()))
+                .thenThrow(DomainException.with(new Error(expectedErrorMessage)));
+
+        final var request = MockMvcRequestBuilders.post("/categories")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(anInput));
+
+        this.mockMvc.perform(request)
+                .andDo(print())
+                .andExpectAll(
+                        status().isUnprocessableEntity(),
+                        header().string("Location", Matchers.nullValue()),
+                        header().string("Content-Type", MediaType.APPLICATION_JSON_VALUE),
+                        jsonPath("$.errors", Matchers.hasSize(1)),
+                        jsonPath("$.errors[0].message", Matchers.equalTo(expectedErrorMessage)),
+                        jsonPath("$.message", Matchers.equalTo(expectedErrorMessage))
+                );
+
+        verify(createCategoryUseCase, times(1)).execute(argThat(cmd ->
+                Objects.equals(expectedName, cmd.name())
+                        && Objects.equals(expectedDescription, cmd.description())
                         && Objects.equals(expectedIsActive, cmd.isActive())
         ));
     }
