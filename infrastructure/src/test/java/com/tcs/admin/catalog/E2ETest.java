3d44a0ff5030bc154705421f61768cfe72767a88
tiagocosta <tiago.costa.silva@gmail.com>
2025-02-18 10:35:51 -0300

    Add basis for e2e test with testcontainers

---
+++ infrastructure/src/test/java/com/tcs/admin/catalog/E2ETest.java
@@ -0,0 +1,14 @@
+package com.tcs.admin.catalog;
+
+import com.tcs.admin.catalog.infrastructure.configuration.WebServerConfig;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@ActiveProfiles("test-e2e")
+@SpringBootTest(classes = WebServerConfig.class)
+public @interface E2ETest {}
