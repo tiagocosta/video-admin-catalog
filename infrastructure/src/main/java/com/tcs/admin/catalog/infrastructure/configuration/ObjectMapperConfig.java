dffd781f53163dbd47437c1280e356b4eca74651
tiagocosta <tiago.costa.silva@gmail.com>
2025-02-17 21:20:34 -0300

    Add and configure ObjectMapper

--- infrastructure/src/main/java/com/tcs/admin/catalog/infrastructure/configuration/ObjectMapperConfig.java
+++ infrastructure/src/main/java/com/tcs/admin/catalog/infrastructure/configuration/ObjectMapperConfig.java
@@ -1,4 +1,15 @@
 package com.tcs.admin.catalog.infrastructure.configuration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tcs.admin.catalog.infrastructure.configuration.json.Json;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class ObjectMapperConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return Json.mapper();
+    }
 }
