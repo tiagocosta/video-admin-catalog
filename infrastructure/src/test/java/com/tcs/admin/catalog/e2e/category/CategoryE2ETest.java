47afb9370b754a44c2f9a4e53fe3d94c22675faa
tiagocosta <tiago.costa.silva@gmail.com>
2025-02-18 11:55:35 -0300

    Add e2e test for category listing

--- infrastructure/src/test/java/com/tcs/admin/catalog/e2e/category/CategoryE2ETest.java
+++ infrastructure/src/test/java/com/tcs/admin/catalog/e2e/category/CategoryE2ETest.java
@@ -6,19 +6,25 @@ import com.tcs.admin.catalog.infrastructure.category.models.CategoryResponse;
 import com.tcs.admin.catalog.infrastructure.category.models.CreateCategoryRequest;
 import com.tcs.admin.catalog.infrastructure.category.persistence.CategoryRepository;
 import com.tcs.admin.catalog.infrastructure.configuration.json.Json;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @E2ETest
 @Testcontainers
@@ -42,6 +48,11 @@ public class CategoryE2ETest {
         registry.add("mysql.port", () -> MYSQL_CONTAINER.getMappedPort(3306));
     }
 
+    @BeforeEach
+    void cleanUp() {
+        this.categoryRepository.deleteAll();
+    }
+
     @Test
     public void as_aCatalogAdmin_shouldBeAbleToCreateNewCategory() throws Exception {
         Assertions.assertTrue(MYSQL_CONTAINER.isRunning());
@@ -63,6 +74,111 @@ public class CategoryE2ETest {
         Assertions.assertNull(actualCategory.deletedAt());
     }
 
+    @Test
+    public void as_aCatalogAdmin_shouldBeAbleToNavigateAllCategories() throws Exception {
+        Assertions.assertTrue(MYSQL_CONTAINER.isRunning());
+        Assertions.assertEquals(0, this.categoryRepository.count());
+
+        final var actualId1 = givenACategory("Movies", null, true);
+        final var actualId2 = givenACategory("Documentaries", null, true);
+        final var actualId3 = givenACategory("Series", null, true);
+
+        listCategories(0, 1)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.current_page", Matchers.equalTo(0)))
+                .andExpect(jsonPath("$.per_page", Matchers.equalTo(1)))
+                .andExpect(jsonPath("$.total", Matchers.equalTo(3)))
+                .andExpect(jsonPath("$.items", Matchers.hasSize(1)))
+                .andExpect(jsonPath("$.items[0].name", Matchers.equalTo("Documentaries")));
+
+        listCategories(1, 1)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.current_page", Matchers.equalTo(1)))
+                .andExpect(jsonPath("$.per_page", Matchers.equalTo(1)))
+                .andExpect(jsonPath("$.total", Matchers.equalTo(3)))
+                .andExpect(jsonPath("$.items", Matchers.hasSize(1)))
+                .andExpect(jsonPath("$.items[0].name", Matchers.equalTo("Movies")));
+
+        listCategories(2, 1)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.current_page", Matchers.equalTo(2)))
+                .andExpect(jsonPath("$.per_page", Matchers.equalTo(1)))
+                .andExpect(jsonPath("$.total", Matchers.equalTo(3)))
+                .andExpect(jsonPath("$.items", Matchers.hasSize(1)))
+                .andExpect(jsonPath("$.items[0].name", Matchers.equalTo("Series")));
+
+        listCategories(3, 1)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.current_page", Matchers.equalTo(3)))
+                .andExpect(jsonPath("$.per_page", Matchers.equalTo(1)))
+                .andExpect(jsonPath("$.total", Matchers.equalTo(3)))
+                .andExpect(jsonPath("$.items", Matchers.hasSize(0)));
+    }
+
+    @Test
+    public void as_aCatalogAdmin_shouldBeAbleToSearchBetweenAllCategories() throws Exception {
+        Assertions.assertTrue(MYSQL_CONTAINER.isRunning());
+        Assertions.assertEquals(0, this.categoryRepository.count());
+
+        final var actualId1 = givenACategory("Movies", null, true);
+        final var actualId2 = givenACategory("Documentaries", null, true);
+        final var actualId3 = givenACategory("Series", null, true);
+
+        listCategories(0, 1, "mov")
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.current_page", Matchers.equalTo(0)))
+                .andExpect(jsonPath("$.per_page", Matchers.equalTo(1)))
+                .andExpect(jsonPath("$.total", Matchers.equalTo(1)))
+                .andExpect(jsonPath("$.items", Matchers.hasSize(1)))
+                .andExpect(jsonPath("$.items[0].name", Matchers.equalTo("Movies")));
+    }
+
+    @Test
+    public void as_aCatalogAdmin_shouldBeAbleToSortAllCategoriesByDescriptionDesc() throws Exception {
+        Assertions.assertTrue(MYSQL_CONTAINER.isRunning());
+        Assertions.assertEquals(0, this.categoryRepository.count());
+
+        final var actualId1 = givenACategory("Movies", "Movies", true);
+        final var actualId2 = givenACategory("Documentaries", "Docs", true);
+        final var actualId3 = givenACategory("Series", "Series", true);
+
+        listCategories(0, 3, "", "description", "desc")
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.current_page", Matchers.equalTo(0)))
+                .andExpect(jsonPath("$.per_page", Matchers.equalTo(3)))
+                .andExpect(jsonPath("$.total", Matchers.equalTo(3)))
+                .andExpect(jsonPath("$.items", Matchers.hasSize(3)))
+                .andExpect(jsonPath("$.items[0].name", Matchers.equalTo("Series")))
+                .andExpect(jsonPath("$.items[1].name", Matchers.equalTo("Movies")))
+                .andExpect(jsonPath("$.items[2].name", Matchers.equalTo("Documentaries")));
+    }
+
+    private ResultActions listCategories(final int page, final int perPage) throws Exception {
+        return listCategories(page, perPage, "", "", "");
+    }
+
+    private ResultActions listCategories(final int page, final int perPage, final String search) throws Exception {
+        return listCategories(page, perPage, search, "", "");
+    }
+
+    private ResultActions listCategories(
+            final int page,
+            final int perPage,
+            final String search,
+            final String sort,
+            final String direction
+    ) throws Exception {
+        final var aRequest = MockMvcRequestBuilders.get("/categories")
+                .queryParam("page", String.valueOf(page))
+                .queryParam("perPage", String.valueOf(perPage))
+                .queryParam("search", search)
+                .queryParam("sort", sort)
+                .queryParam("direction", direction)
+                .contentType(MediaType.APPLICATION_JSON);
+
+        return this.mvc.perform(aRequest);
+    }
+
     private CategoryID givenACategory(final String aName, final String aDescription, final boolean isActive) throws Exception {
         final var createCategoryRequest = new CreateCategoryRequest(aName, aDescription, isActive);
 
@@ -71,7 +187,7 @@ public class CategoryE2ETest {
                 .content(Json.writeValueAsString(createCategoryRequest));
 
         final var actualId = this.mvc.perform(aRequest)
-                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(status().isCreated())
                 .andReturn()
                 .getResponse().getHeader("Location")
                 .replace("/categories/", "");
@@ -84,7 +200,7 @@ public class CategoryE2ETest {
                 .contentType(MediaType.APPLICATION_JSON);
 
         final var json = this.mvc.perform(aRequest)
-                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(status().isOk())
                 .andReturn()
                 .getResponse().getContentAsString();
 
