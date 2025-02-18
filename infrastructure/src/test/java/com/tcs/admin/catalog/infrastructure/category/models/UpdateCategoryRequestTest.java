74dd6b3e072a9a4b70038b2d9c692d762e280178
tiagocosta <tiago.costa.silva@gmail.com>
2025-02-17 22:10:21 -0300

    Add marshall/unmarshall tests for UpdateCategoryRequest

---
+++ infrastructure/src/test/java/com/tcs/admin/catalog/infrastructure/category/models/UpdateCategoryRequestTest.java
@@ -0,0 +1,43 @@
+package com.tcs.admin.catalog.infrastructure.category.models;
+
+import com.tcs.admin.catalog.JacksonTest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.json.JacksonTester;
+
+import java.time.Instant;
+
+@JacksonTest
+public class UpdateCategoryRequestTest {
+
+    @Autowired
+    private JacksonTester<UpdateCategoryRequest> json;
+
+    @Test
+    public void testUnmarshall() throws Exception {
+        final var expectedName = "Movies";
+        final var expectedDescription = "Most watched";
+        final var expectedIsActive = true;
+
+        final var json = """
+        {
+            "name": "%s",
+            "description": "%s",
+            "is_active": "%s"
+        }
+        """.formatted(
+                expectedName,
+                expectedDescription,
+                expectedIsActive
+        );
+
+        final var actualJson = this.json.parse(json);
+
+        Assertions.assertThat(actualJson)
+                .hasFieldOrPropertyWithValue("name", expectedName)
+                .hasFieldOrPropertyWithValue("description", expectedDescription)
+                .hasFieldOrPropertyWithValue("active", expectedIsActive)
+        ;
+    }
+}
