5eecdf9e1f659d263a03348dfbe2e318a9fb1af1
tiagocosta <tiago.costa.silva@gmail.com>
2025-02-21 15:53:54 -0300

    Add Fixture to encapsulate and extend javafaker

---
+++ application/src/test/java/com/tcs/admin/catalog/application/Fixture.java
@@ -0,0 +1,21 @@
+package com.tcs.admin.catalog.application;
+
+import com.github.javafaker.Faker;
+import com.tcs.admin.catalog.domain.castmember.CastMemberType;
+
+public final class Fixture {
+
+    private static final Faker FAKER = new Faker();
+
+    public static String name() {
+        return FAKER.name().fullName();
+    }
+
+    public static final class CastMember {
+
+        public static CastMemberType type() {
+            return FAKER.options()
+                    .option(CastMemberType.ACTOR, CastMemberType.DIRECTOR);
+        }
+    }
+}
