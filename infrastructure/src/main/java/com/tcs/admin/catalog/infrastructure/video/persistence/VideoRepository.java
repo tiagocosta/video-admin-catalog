275a5337045bb08cebffbf63ab4a7774d6b2a38e
tiagocosta <tiago.costa.silva@gmail.com>
2025-02-27 15:00:38 -0300

    Fix video jpa repository

--- infrastructure/src/main/java/com/tcs/admin/catalog/infrastructure/video/persistence/VideoRepository.java
+++ infrastructure/src/main/java/com/tcs/admin/catalog/infrastructure/video/persistence/VideoRepository.java
@@ -2,7 +2,5 @@ package com.tcs.admin.catalog.infrastructure.video.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
-
-public interface VideoRepository extends JpaRepository<VideoRepository, UUID> {
+public interface VideoRepository extends JpaRepository<VideoJpaEntity, String> {
 }
