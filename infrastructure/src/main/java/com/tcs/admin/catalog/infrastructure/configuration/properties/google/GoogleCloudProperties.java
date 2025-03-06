b81d73e4662addc09caa877d98e718564af813a0
tiagocosta <tiago.costa.silva@gmail.com>
2025-03-06 11:19:13 -0300

    Move gc properties classes to specific google package

--- infrastructure/src/main/java/com/tcs/admin/catalog/infrastructure/configuration/properties/google/GoogleCloudProperties.java
+++ infrastructure/src/main/java/com/tcs/admin/catalog/infrastructure/configuration/properties/google/GoogleCloudProperties.java
@@ -1,4 +1,4 @@
-package com.tcs.admin.catalog.infrastructure.configuration.properties;
+package com.tcs.admin.catalog.infrastructure.configuration.properties.google;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
